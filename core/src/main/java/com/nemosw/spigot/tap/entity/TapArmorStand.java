749cc1200d663382bb92b427d000a7e17d693f05
nemosw <nemosrc@gmail.com>
2019-08-03 23:48:31 +0900

    armorstand update

--- core/src/main/java/com/nemosw/spigot/tap/entity/TapArmorStand.java
+++ core/src/main/java/com/nemosw/spigot/tap/entity/TapArmorStand.java
@@ -11,16 +11,44 @@ public interface TapArmorStand extends TapLivingEntity
 
     Vector getHeadPos();
 
+    void setMarker(boolean marker);
+
+    boolean isMarker();
+
+    void setBasePlate(boolean basePlate);
+
+    boolean hasBasePlate();
+
+    void setArms(boolean arms);
+
+    boolean hasArms();
+
+    void setSmall(boolean small);
+
+    boolean isSmall();
+
     void setHeadPose(float x, float y, float z);
+
+    Vector getHeadPose();
 
     void setBodyPose(float x, float y, float z);
 
+    Vector getBodyPose();
+
     void setLeftArmPose(float x, float y, float z);
+
+    Vector getLeftArmPose();
 
     void setRightArmPose(float x, float y, float z);
 
+    Vector getRightArmPose();
+
     void setLeftLegPose(float x, float y, float z);
 
+    Vector getLeftLegPose();
+
     void setRightLegPose(float x, float y, float z);
+
+    Vector getRightLegPose();
 
 }
--- v1_12_R1/src/main/java/com/nemosw/spigot/tap/v1_12_R1/entity/NMSArmorStand.java
+++ v1_12_R1/src/main/java/com/nemosw/spigot/tap/v1_12_R1/entity/NMSArmorStand.java
@@ -10,11 +10,6 @@ import org.bukkit.entity.ArmorStand;
 public class NMSArmorStand extends NMSLivingEntity implements TapArmorStand
 {
 
-    private static Vector toVector(Vector3f v)
-    {
-        return new Vector(v.getX(), v.getY(), v.getZ());
-    }
-
     private final EntityArmorStand armorStand;
 
     NMSArmorStand(Entity entity)
@@ -22,6 +17,11 @@ public class NMSArmorStand extends NMSLivingEntity implements TapArmorStand
         super(entity);
 
         this.armorStand = (EntityArmorStand) entity;
+    }
+
+    private static Vector toVector(Vector3f v)
+    {
+        return new Vector(v.getX(), v.getY(), v.getZ());
     }
 
     @Override
@@ -78,4 +78,87 @@ public class NMSArmorStand extends NMSLivingEntity implements TapArmorStand
         this.armorStand.setRightLegPose(new Vector3f(x, y, z));
     }
 
+    @Override
+    public boolean isMarker()
+    {
+        return armorStand.isMarker();
+    }
+
+    @Override
+    public void setMarker(boolean marker)
+    {
+        armorStand.setMarker(marker);
+    }
+
+    @Override
+    public void setBasePlate(boolean basePlate)
+    {
+        armorStand.setBasePlate(basePlate);
+    }
+
+    @Override
+    public boolean hasBasePlate()
+    {
+        return armorStand.hasBasePlate();
+    }
+
+    @Override
+    public void setArms(boolean arms)
+    {
+        armorStand.setArms(arms);
+    }
+
+    @Override
+    public boolean hasArms()
+    {
+        return armorStand.hasArms();
+    }
+
+    @Override
+    public boolean isSmall()
+    {
+        return armorStand.isSmall();
+    }
+
+    @Override
+    public void setSmall(boolean small)
+    {
+        armorStand.setSmall(small);
+    }
+
+    @Override
+    public Vector getHeadPose()
+    {
+        return toVector(armorStand.headPose);
+    }
+
+    @Override
+    public Vector getBodyPose()
+    {
+        return toVector(armorStand.bodyPose);
+    }
+
+    @Override
+    public Vector getLeftArmPose()
+    {
+        return toVector(armorStand.leftArmPose);
+    }
+
+    @Override
+    public Vector getRightArmPose()
+    {
+        return toVector(armorStand.rightArmPose);
+    }
+
+    @Override
+    public Vector getLeftLegPose()
+    {
+        return toVector(armorStand.leftLegPose);
+    }
+
+    @Override
+    public Vector getRightLegPose()
+    {
+        return toVector(armorStand.rightLegPose);
+    }
 }
